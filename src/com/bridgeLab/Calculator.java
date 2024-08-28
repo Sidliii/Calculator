65b3fe537bd2a6245c75ad7a1331a270d9e54ee0
unknown <sidheshthakur18@gmail.com>
2024-08-28 11:05:47 +0530

    Added: Welcome msg is added

---
+++ src/com/bridgeLab/Calculator.java
@@ -0,0 +1,10 @@
+package com.bridgeLab;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to calculator");
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/bridgeLab/EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.bridgeLab;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
